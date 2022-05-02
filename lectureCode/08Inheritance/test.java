d106cc368209b940377ce2afed2ba05724f4c7f1
victory-hlx <a1272012007@163.com>
2022-05-02 21:37:12 +0800

    done with arrayset

---
+++ lectureCode/08Inheritance/test.java
@@ -0,0 +1,15 @@
+public class test {
+
+    public static void main(String[] args) {
+        String keyboard = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+        System.out.println(keyboard.indexOf("q"));
+    }
+}
+/*
+ * @Author: your name
+ * @Date: 2022-04-28 19:19:29
+ * @LastEditTime: 2022-04-28 19:25:21
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: \08Inheritance\test.java
+ */
