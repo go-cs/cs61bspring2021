f84ef1204da27d5239aa349e427909fd0400d929
victory-hlx <a1272012007@163.com>
2022-05-03 21:47:43 +0800

    done with arrayset

--- lectureCode/11Set/ArraySet.java
+++ lectureCode/11Set/ArraySet.java
@@ -1,8 +1,83 @@
+import java.util.Iterator;
+
 /*
- * @Author: your name
- * @Date: 2022-05-02 16:11:41
- * @LastEditTime: 2022-05-02 16:11:43
- * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- * @FilePath: \11Set\ArraySet.java
+ *
  */
+public class ArraySet<T> implements Iterable<T> {
+    private T[] items;
+    private int size;
+
+    public ArraySet() {
+        items = (T[]) new Object[100];
+        size = 0;
+    }
+
+    public boolean contains(T x) {
+        for (int i = 0; i < size; i++) {
+            if (x.equals(items[i])) {
+                return true;//items[i].equals(x)
+            }
+        }
+        return false;
+    }
+
+    public void add(T x) {
+        if (x == null) {
+            throw new IllegalArgumentException("You can not add null in the ArraySet.");
+        }
+        if (!contains(x)) {
+            items[size] = x;
+            size += 1;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public Iterator<T> iterator() {
+        return new ArraySetIterator();
+    }
+
+    private class ArraySetIterator implements Iterator<T> {
+        private int wizPos;
+
+        public ArraySetIterator() {
+            wizPos = 0;
+        }
+
+        public boolean hasNext() {
+            return wizPos < size;
+        }
+
+        public T next() {
+            T returnItem = items[wizPos];
+            wizPos += 1;
+            return returnItem;
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        ArraySet<Integer> aset = new ArraySet<>();
+        aset.add(5);
+        aset.add(23);
+        aset.add(42);
+        for (int i : aset) {
+            System.out.println(i);
+        }
+
+        /**equals the above*/
+        //ArraySet<Integer> jaset = new ArraySet<>();
+//        Iterator<Integer> seer = aset.iterator();
+//
+//        while (seer.hasNext()) {
+//            System.out.println(seer.next());
+//        }
+
+    }
+
+}
