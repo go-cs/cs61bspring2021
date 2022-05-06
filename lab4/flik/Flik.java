14a1f8ff3b22b8c31a737522bdd589f7384e8c6d
victory-hlx <a1272012007@163.com>
2022-05-06 16:25:30 +0800

    finished lab4, flik works

--- lab4/flik/Flik.java
+++ lab4/flik/Flik.java
@@ -1,13 +1,19 @@
 package flik;
 
-/** An Integer tester created by Flik Enterprises.
+/**
+ * An Integer tester created by Flik Enterprises.
+ *
  * @author Josh Hug
- * */
+ */
 public class Flik {
-    /** @param a Value 1
-     *  @param b Value 2
-     *  @return Whether a and b are the same */
+    /**
+     * @param a Value 1
+     * @param b Value 2
+     * @return Whether a and b are the same
+     */
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        return a.equals(b);
+        //return a == b;
+        //在i=j=128时存储地址不一样了，128893786 VS 1732398722
     }
 }
